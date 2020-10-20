c4d5dbf831d27a646e009cb9e82a2145f3e18d46
Deepak Nayak <deepaknayak7832@gmail.com>
2020-10-20 23:51:19 +0530

    index files

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import C1 from './components/metrics/C1';
+import C2 from './components/metrics/C2';
+import C3 from './components/metrics/C3';
+import C4 from './components/metrics/C4';
+import C5 from './components/metrics/C5';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <C1 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
